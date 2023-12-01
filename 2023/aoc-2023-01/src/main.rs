cc5a7805f5ba9dca3070c467ac6bb9e2b1b26b13
Deixel <Deixel@users.noreply.github.com>
2023-12-01 20:55:04 +0000

    Add reading from file and spliting into lines

--- 2023/aoc-2023-01/src/main.rs
+++ 2023/aoc-2023-01/src/main.rs
@@ -1,3 +1,13 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "/home/deixel/Projects/advent_of_code/2023/aoc-2023-01/src/input.txt";
+
+    let contents = fs::read_to_string(file_path).expect("Can't read the file");
+
+    let lines = contents.split("\n");
+
+    for line in lines {
+        println!("{line}");
+    }
 }
