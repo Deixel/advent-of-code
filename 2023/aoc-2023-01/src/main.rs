54c6bb68cb652a03e55b654de7a518fc318fe34a
Deixel <Deixel@users.noreply.github.com>
2023-12-02 22:09:29 +0000

    Refactor ready for part 2

--- 2023/aoc-2023-01/src/main.rs
+++ 2023/aoc-2023-01/src/main.rs
@@ -1,5 +1,6 @@
-use std::fs;
+use once_cell::sync::Lazy;
 use regex::Regex;
+use std::fs;
 
 fn main() {
     let file_path = "/home/deixel/Projects/advent_of_code/2023/aoc-2023-01/src/input.txt";
@@ -8,34 +9,32 @@ fn main() {
 
     let lines = contents.split("\n");
 
-    let regex = Regex::new(r"(\d)").unwrap();
-
-    let mut numbers:Vec<Vec<&str>> = vec![];
-    for line in lines {
-        let captured_numbers = regex.captures_iter(line);
-        let mut numbers_from_line: Vec<&str> = vec![];
-            for number in captured_numbers {
-            let num_str = number.get(0).unwrap().as_str();
-            // let num = num_str.parse::<u32>().unwrap();
-
-            numbers_from_line.push(num_str);
-        }
-        numbers.push(numbers_from_line);
-    }
-
     let mut total = 0;
-    for line in numbers {
-        let first_number = line.first().unwrap();
-        let last_number = line.last().unwrap();
-
-        let mut final_number_str :String = first_number.to_string();
-        final_number_str.push_str(last_number);
-
-        let final_number: u32 = final_number_str.parse().unwrap();
-        total += final_number;
-
+    for line in lines {
+        let numbers_from_line = extract_numbers_from_line(line);
+        total += numbers_from_line_to_number(numbers_from_line);
     }
+    println!("Calibration: {total}"); // 56397
+}
 
-    println!("{total}");
+fn extract_numbers_from_line(line: &str) -> Vec<&str> {
+    static numbers_regex: Lazy<Regex> = Lazy::new(|| Regex::new(r"(\d)").unwrap());
 
+    let captured_numbers = numbers_regex.captures_iter(line);
+    let mut numbers_from_line: Vec<&str> = vec![];
+    for number in captured_numbers {
+        let num_str = number.get(0).unwrap().as_str();
+
+        numbers_from_line.push(num_str);
+    }
+    numbers_from_line
+}
+
+fn numbers_from_line_to_number(numbers_from_line: Vec<&str>) -> u32 {
+    let first_number = numbers_from_line.first().unwrap();
+    let last_number = numbers_from_line.last().unwrap();
+    let mut final_number_str: String = first_number.to_string();
+    final_number_str.push_str(last_number);
+
+    final_number_str.parse().unwrap()
 }
