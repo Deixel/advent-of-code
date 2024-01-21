9e543ced039d6d1013b3646e201c021f50742a2b
Deixel <Deixel@users.noreply.github.com>
2024-01-21 17:41:54 +0000

    Day 3

--- 2023/aoc-2023-03/src/main.rs
+++ 2023/aoc-2023-03/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use aoc_util::load_input;
+use once_cell::sync::Lazy;
+use regex::Regex;
+
+struct Number {
+    value: usize,
+    length: usize,
 }
+
+enum Tile {
+    Empty,
+    Symbol,
+    Number(Number),
+}
+
+fn main() {
+    let lines = load_input("./src/input.txt");
+
+    for line in lines {
+        println!("{line}");
+    }
+}
+
+fn is_symbol(str: &str) -> bool {
+    static SYMBOL_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"[^.\d]").unwrap());
+    SYMBOL_REGEX.is_match(str)
+}
+
+fn search_left(line: &str, start_idx: usize, power: usize) -> usize {}
